46fd495c16e566bb581de3c0891879a6549aade5
devaharsha <deva.harsha17@gmail.com>
2015-03-09 09:19:51 -0400

    added frames

---
+++ WpfApplication1/WpfApplication1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class Binary
+    {
+
+        public float prevtot;
+        public string oper= "+";
+        public float operand;
+        public string result;
+
+        public float getres;
+    
+        switch ()
+	{
+		default:
+	}
+
+    }
+    class Class1
+    {
+    }
+}
